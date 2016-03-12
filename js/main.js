5eccbcc73434c7bf392062f00537aacc5bccc2fd
panickylemon <alenka__1992@mail.ru>
2016-03-12 12:29:52 +0300

    menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+$(document).ready(function(){
+
+    $(".menuToggle").click(function() {
+        $(".menuToggle").hide();
+        $(".menuOpen").toggle("slide", {direction:"right"}, 500);
+        $(".menuClose").delay(400).fadeIn(100);
+    });
+
+    $(document).click(function (e){ // событие клика по веб-документу
+        var menu = $(".menuOpen");
+        var burger = $(".menuToggle");
+        if (!menu.is(e.target) && !burger.is(e.target)// если клик был не по нашему блоку
+            && menu.has(e.target).length === 0
+        && menu.is(':visible')) { // и не по его дочерним элементам
+            $(".menuClose").hide();
+            burger.show();
+            menu.toggle("slide", {direction:"right"}, 500);
+        }
+    });
+});
 
